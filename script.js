d08aab6673032ca6c5450669052cd5c5a89f560e
AndreiaContes <andreiacontes@hotmail.com>
2026-04-29 02:21:52 -0300

    Refactor carousel and back-to-top functionality

--- script.js
+++ script.js
@@ -35,9 +35,3 @@ if (backTop) {
     });
   });
 }
-const menuToggle = document.getElementById("menu-toggle");
-const navbar = document.querySelector(".navbar");
-
-menuToggle.addEventListener("click", () => {
-  navbar.classList.toggle("active");
-});
