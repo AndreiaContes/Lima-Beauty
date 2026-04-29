1203f1e99525a5407b29200046c7e7ef2bfbe035
AndreiaContes <andreiacontes@hotmail.com>
2026-04-29 02:19:49 -0300

    Add menu toggle functionality to navbar

--- script.js
+++ script.js
@@ -35,3 +35,9 @@ if (backTop) {
     });
   });
 }
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.querySelector(".navbar");
+
+menuToggle.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
